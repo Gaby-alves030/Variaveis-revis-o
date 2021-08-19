3ea8873b4541342e4ff09a5a7a2f6d1a3dbb641d
Gaby <gaabyrocha574@gmail.com>
2021-08-19 17:57:22 +0000

    declaração de variaveis

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,3 +1,12 @@
 public class BalancoTrimestral{
-    
+    public static void main(String args[]){
+        int gastosJaneiros=1500;
+        int gastosFevereiro=23000;
+        int gastoMarço=17000;
+        int gastosTrimestral=gastosJaneiro+Gastosfevereiro+gastosMarço;
+        System.out.println(gastosTrimestral);
+        int mediaMensal=gastosTrimestral/3;
+        System.out.println(mediaMensal);
+    }
+
 }
